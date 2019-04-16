c7b161aeabe869d236f5323d43d7f971f92f4a68
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-16 18:29:01 +0100

    Adds comments to notes controller file

--- note-controller.js
+++ note-controller.js
@@ -1,15 +1,23 @@
-(function() {
-  function NoteController(list) {
+(function(exports) {
+  function NoteController(list = new NoteList) {
     this.noteList = list; //note list
-    this.noteList.addNote("Favourite drinke: seltzer"); //adds note
-    this.noteList.addNote("Favourite drinke: cola"); //adds note
-    this.noteListView = new NoteListView(this.noteList.list); //note list view
+    this.noteList.addNote("Favourite drink: tea"); //adds note
+    this.noteList.addNote("Favourite food: sushi"); //adds note
+    this.noteListView = new NoteListView(this.noteList.showList()); //note list view
   }
 
-  NoteController.prototype.insertHTML = function() {
+  NoteController.prototype.insertHtml = function() {
     document.getElementById("app").innerHTML = this.noteListView.outputHtml(); //html from note list view
   };
 
-  var controller = new NoteController(new NoteList());
-  controller.insertHTML();
+  // var controller = new NoteController(new NoteList());
+  // controller.insertHtml();
+
+  exports.NoteController = NoteController
 })(this);
+
+// testThingIsNoteControllerObject()
+// testThingTakesNoteListAsParameter()
+// testConstructorAddsNote()
+// testConstructorCreatesNoteListView()
+// does something with insertHtml()
